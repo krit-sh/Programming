a9e044a50fcf63953292e2967263d59d682c6d3d
Ritesh Kumar <kritesh1401@gmail.com>
2024-10-19 13:13:37 +0530

    Add files via upload

---
+++ codechef/sweets_shop.py
@@ -0,0 +1,13 @@
+x, n= map(int, input().split())
+
+money_left = x - (n*10)
+print(round(money_left/20))
+
+# x, n = map(int, input().split())
+# lc=10
+# jc=20
+# lef = x - (n * lc)
+# if(lef < 0):
+#     print("0")
+# else:
+#     print(round(lef /jc))
